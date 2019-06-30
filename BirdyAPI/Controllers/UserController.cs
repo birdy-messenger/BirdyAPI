f0cdce36a368ac1d9c8f236478d408aa385c3716
Vlad <hrrrrustic@gmail.com>
2019-06-30 19:41:18 +0300

    Add UserController and UserService instead of userget

--- BirdyAPI/Controllers/UserController.cs
+++ BirdyAPI/Controllers/UserController.cs
@@ -2,45 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BirdyAPI.Dto;
+using BirdyAPI.Services;
+using BirdyAPI.Tools;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BirdyAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/user")]
     public class UserController : Controller
     {
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        private readonly UserService _userService;
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        public UserController(UserContext context)
         {
-            return "value";
+            _userService = new UserService(context);
         }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult GetUserInfo([FromQuery] UserSessionDto user)
         {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            try
+            {
+                return Ok(_userService.SearchUserInfo(user));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.SerializeAsResponse());
+            }
         }
     }
 }
---
+++ BirdyAPI/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirdyAPI.DataBaseModels;
+using BirdyAPI.Dto;
+
+namespace BirdyAPI.Services
+{
+    public class UserService
+    {
+        private readonly UserContext _context;
+
+        public UserService(UserContext context)
+        {
+            _context = context;
+        }
+
+        public UserAccountDto SearchUserInfo(UserSessionDto user)
+        {
+            User currentUser = _context.Users.FirstOrDefault(k => k.Id == user.Id);
+            if (currentUser == null)
+                throw new ArgumentException("User Not Found");
+            else
+            {
+                if (IsTokenValid(currentUser, user.Token))
+                    return new UserAccountDto(currentUser.FirstName, currentUser.AvatarReference);
+                else
+                    throw new ArgumentException("Invalid Token");
+            }
+        }
+        private bool IsTokenValid(User user, int token)
+        {
+            if (user.Token == token)
+                return true;
+            else
+                return false;
+        }
+    }
+}
